28a5c320a58d382a07c9731ece5a2bc9d98018cf
danah olivetree <danah.olivetree@gmail.com>
2017-08-17 10:33:16 -0600

    layout fixed, js works

--- cart.js
+++ cart.js
@@ -7,21 +7,21 @@ var menu = {
   'ice_cream': 7.99
 }
 //let cart = document.getElementById("shoppingCart") //this is the UL we add li to
-let cartTotal= 0.0
-let taxVal = 0
-let totalVal = 0
+var cartTotal = 0
+var taxVal = 0
+var totalVal = 0
 let sub = document.getElementById('subtotal')
 let subtext = document.createTextNode('')//)
 sub.appendChild(subtext);
 let taxText = document.createTextNode('')//taxVal)
-let tax= document.getElementById('tax')
+let tax = document.getElementById('tax')
 tax.appendChild(taxText)
 let total = document.getElementById('total')
 let totalText = document.createTextNode('')//totalVal)
 total.appendChild(totalText)
 
 function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function putInCart(event){
@@ -32,37 +32,30 @@ function putInCart(event){
   let tr = document.createElement('tr')
   let tdi = document.createElement('td')
   let tdii = document.createElement('td')
-  tdii.setAttribute("class", "right")
   newItem = tdi.appendChild(newItem)
   newPrice = tdii.appendChild(newPrice)
   tr.appendChild(tdi)
   tr.appendChild(tdii)
+  //tdii = tdii.setAttribute("class", "right")
   body.appendChild(tr)
   let p = parseFloat(menu[selectedItem])
   updateTotals(p);
 }
 
 function updateTotals(inputPrice) {
-
+  let inp = 0
+  inp = inputPrice
+  console.log(typeof inp)
   cartTotal += inputPrice
-  sub.innerHTML = cartTotal
-  taxVal = (parseFloat(taxVal) + parseFloat((inputPrice*.029))).toFixed(2)
+  console.log(typeof cartTotal)
+  sub.innerHTML = cartTotal.toFixed(2)
+  taxVal = (parseFloat(taxVal) + parseFloat(inp*.029)).toFixed(2)
+  console.log("why is my taxVal a ", typeof taxVal)
   tax.innerHTML = taxVal
-  totalVal = (cartTotal + parseFloat(taxVal)).toFixed(2)
-  total.innerHTML = totalVal
+  totalVal = cartTotal + parseFloat(taxVal)
+  console.log(typeof totalVal)
+  total.innerHTML = totalVal.toFixed(2)
 }
-
-// function putInCart(event){
-//   let selectedItem = event.target.getAttribute('id')
-//   newItem = document.createTextNode(capitalize(selectedItem))
-//   slot = document.getElementById("first-item")
-//   newItem = slot.appendChild(newItem)
-//   cartTotal += menu[selectedItem]
-//   priceSlot = document.getElementById("first-item-price")
-//   price = document.createTextNode(menu[selectedItem])
-//   price = priceSlot.appendChild(price)
-//   console.log(newItem)
-// }
 
 let burgerBtn = document.getElementById("burger")
 burgerBtn.addEventListener("click", putInCart)
@@ -74,17 +67,6 @@ let ribsBtn = document.getElementById("ribs")
 ribsBtn.addEventListener("click", putInCart)
 
 
-
-
-
-
-// function getTotal() {
-//   for (let i=0; i<shoppingCart.length; i++) {
-//     for (var k in shoppingCart[i]) {
-//     cartTotal += shoppingCart[i][k] | 0
-//     }
-//   }
-// }
 
 function displayTotal() {
 
